d6acd276219b24d688d24391f020487f2c68d8af
zSnails <erizojuan33@gmail.com>
2022-12-24 15:54:51 -0600

    Remove useless brackets

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -69,10 +69,6 @@ cmp.setup({
 cmp.setup.filetype('gitcommit', {
     sources = cmp.config.sources({
         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
         { name = 'buffer' },
-        
     })
 })
-
-
