f7e479fdf9c35b0526fb7a25a993723d13d465fd
zSnails <erizojuan33@gmail.com>
2022-12-24 14:06:05 -0600

    Add text object configuration to tree sitter configs

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,8 +1,19 @@
 require("nvim-treesitter.configs").setup {
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+
+            keymaps = {
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+            }
+        },
+    },
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = {'org'},
+        additional_vim_regex_highlighting = { 'org' },
     },
 
-    ensure_installed = {'org'},
+    ensure_installed = { 'org' },
 }
