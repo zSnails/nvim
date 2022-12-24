3b48522b7dba48b27dcc2a85d2de90f68c00f31f
zSnails <erizojuan33@gmail.com>
2022-12-24 14:05:12 -0600

    Remove unused function

--- lua/config/current_server.lua
+++ lua/config/current_server.lua
@@ -1,22 +1,5 @@
 local M = {}
 
-function M.serverProgress()
-    local messages = vim.lsp.util.get_progress_messages()
-    if #messages == 0 then
-        return
-    end
-
-    local status = {}
-    for _, msg in pairs(messages) do
-        table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
-    end
-
-    local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-    local ms = vim.loop.hrtime() / 1000000
-    local frame = math.floor(ms / 120) % #spinners
-    return table.concat(status, " | ") .. " " .. spinners[frame + 1]
-end
-
 function M.currentServer()
     return vim.lsp.buf_get_clients()[1].name
 end
