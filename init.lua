d51822ecb8f73b4e0646712e3189209a5076e8cf
zSnails <erizojuan33@gmail.com>
2022-12-25 22:20:28 -0600

    Move everything outside of init.lua

--- init.lua
+++ init.lua
@@ -1,44 +1,11 @@
 ---@diagnostic disable: need-check-nil
-local seasonal = require('extras.seasonal_themes')
-
-
--- vim.opt.syntax = "no"
-
--- NOTE: globals
-vim.g.vimspector_enable_mappings = 'HUMAN'
-
-vim.cmd [[ packadd termdebug ]]
-
-vim.g.termdebug_wide = 1
-
--- NOTE: options
-vim.o.laststatus = 3
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.scrolloff = 7
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.showtabline = false
-vim.o.background = 'dark'
-vim.o.termguicolors = true
-vim.o.showmode = false
-vim.o.showcmd = false
-vim.o.clipboard = 'unnamed'
-vim.o.ruler = false
-vim.o.wrap = false
-vim.o.conceallevel = 3
-vim.g.NERDTreeMinimalUI = 1
-
-vim.o.signcolumn = "yes"
-
--- NOTE: for my future self, vim.o and vim.opt behave different in the sense that
--- vim.o can only allow for basic usage, while vim.opt can do more stuff
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 -- after plugin initialization
+require('config.setup')
+require('config.todo-comments')
+require('config.vimspector')
 require('config.plugins')
-require('config.seasonal')
+-- require('config.seasonal')
 require('config.mappings')
 require('config.lualine')
 require('config.language-servers')
@@ -49,24 +16,4 @@ require('config.completion')
 require('config.treesitter')
 require('config.orgmode')
 require('config.leap')
-
--- these highlight groups disable all diff and window separator background
--- colors so that they're not messing with my beautiful color scheme setup
-vim.cmd [[
-highlight WinSeparator guibg=NONE
-highlight diffadded guibg=NONE
-highlight DiffChange guibg=NONE
-highlight diffremoved guibg=NONE
-]]
-
--- NOTE: todo-comments keybinds and config
-require("todo-comments").setup {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    signs = true
-}
-
-local theme = seasonal.getTheme()
-print(theme)
-vim.cmd.colorscheme(theme)
+require('config.termdebug')
