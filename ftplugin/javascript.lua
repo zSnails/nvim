2f8c148c5ac2314d6c0e9b31c78f0fcbd90e7b16
zSnails <erizojuan33@gmail.com>
2022-12-25 22:21:20 -0600

    Add sane defaults for javascript

---
+++ ftplugin/javascript.lua
@@ -0,0 +1,4 @@
+-- NOTE: this is just a meme lmao, I must remember to remove this
+-- if I ever want to do web development or some shit like that, at
+-- least I know my NodeJS days are long gone by now.
+vim.cmd ":q"
