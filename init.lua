feec083d15b9c6c5f18232a6710a49a671541c0e
zSnails <erizojuan33@gmail.com>
2023-09-25 05:12:30 -0600

    Updates error message

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@
 
 local version = vim.version()
 if not vim.fn.has("nvim-0.8") then
-    vim.api.nvim_err_writeln(string.format("This configuration requires nvim 0.8+, but you're using %d.%d.%d", version
+    vim.api.nvim_err_writeln(string.format(
+        "[zSnails/nvim]: This configuration requires nvim 0.8+, but you're using %d.%d.%d", version
         .major, version.minor, version.patch))
     return
 end
