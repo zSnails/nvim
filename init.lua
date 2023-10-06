7f27e53dfb108ac5402639a13d87c0945650780c
zSnails <erizojuan33@gmail.com>
2023-10-06 09:05:36 +0000

    test

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ require('config.feline')
 require('config.todo-comments')
 require('config.mappings')
 require('config.language-servers')
+
 require('config.gitsigns')
 require('config.telescope')
 require('config.nvimtree')
