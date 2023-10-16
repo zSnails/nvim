b1d8f5c636ff07e24e3e39e9655067a1edcbaed9
zSnails <erizojuan33@gmail.com>
2023-10-15 20:28:11 -0600

    Make it so that textwidth also words on windows

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,7 +1,5 @@
-if vim.loop.os_uname().sysname == "Windows_NT" then
-    return
-end
-
 vim.opt_local.textwidth = 85
-vim.opt_local.spell = true
-vim.opt_local.spelllang = 'es'
+if vim.loop.os_uname().sysname ~= "Windows_NT" then
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'es'
+end
