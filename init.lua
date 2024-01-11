b06e867fb8e30832052e0b86ca3f7fbb3aed9277
zSnails <erizojuan33@gmail.com>
2024-01-10 21:04:49 -0600

    fix: Bump required version from 0.8 -> 0.9

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- Initialize everything
 
 local version = vim.version()
-if not vim.fn.has("nvim-0.8") then
+if not vim.fn.has("nvim-0.9") then
     vim.api.nvim_err_writeln(string.format(
-        "[zSnails/nvim]: This configuration requires nvim 0.8+, but you're using %d.%d.%d", version
+        "[zSnails/nvim]: This configuration requires nvim 0.9+, but you're using %d.%d.%d", version
         .major, version.minor, version.patch))
     return
 end
