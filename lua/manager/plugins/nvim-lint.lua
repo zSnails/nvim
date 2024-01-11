ecaa152aac570b4dd7febc91978b3cc51b124b96
zSnails <erizojuan33@gmail.com>
2024-01-10 22:30:45 -0600

    feat: Add linting capabilities.

---
+++ lua/manager/plugins/nvim-lint.lua
@@ -0,0 +1,16 @@
+return {
+    'mfussenegger/nvim-lint',
+    config = function()
+        local lint = require('lint')
+        lint.linters_by_ft = {
+            go = { "golangcilint" },
+            python = { "mypy" }
+        }
+        vim.keymap.set("n", "<leader>ll", lint.try_lint, { silent = true })
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+            callback = function()
+                lint.try_lint()
+            end
+        })
+    end
+}
