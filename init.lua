06a661b6a435ffb3c3b5913fcfbbd4192fbd8b31
zSnails <erizojuan33@gmail.com>
2024-12-08 04:19:58 -0600

    fix: use `vim.system` instead of `vim.fn.system` (and await it)

--- init.lua
+++ init.lua
@@ -10,14 +10,14 @@ end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-    vim.fn.system({
+    vim.system({
         "git",
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
         "--branch=stable",
         lazypath
-    })
+    }):wait()
 end
 vim.opt.rtp:prepend(lazypath)
 
