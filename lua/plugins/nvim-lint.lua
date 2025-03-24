08bd93024afbacab7d614c0b5742135ad25c10e4
zSnails <erizojuan33@gmail.com>
2025-03-24 10:58:32 -0600

    feat: keybind to launch the quickfix list with linter messages

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,9 +5,10 @@ return {
         local lint = require('lint')
         lint.linters_by_ft = {
             go = { "golangcilint" },
-            python = { "mypy" }
+            python = { "mypy", "pylint" }
         }
         vim.keymap.set("n", "<leader>ll", lint.try_lint, { silent = true })
+        vim.keymap.set("n", "<leader>di", vim.diagnostic.setqflist)
         -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         --     callback = function()
         --         lint.try_lint()
