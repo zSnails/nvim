e81da3ccbc864c4bf1879cd543fe3bec3c69f3d3
zSnails <erizojuan33@gmail.com>
2025-08-08 19:01:39 -0600

    fix: new sensible defaults for my own sanity

--- lua/config/setup.lua
+++ lua/config/setup.lua
@@ -2,6 +2,7 @@ vim.o.laststatus = 3
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 7
+vim.o.sidescrolloff = 8
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
@@ -12,6 +13,8 @@ vim.o.showmode = false
 vim.o.showcmd = true
 vim.o.showcmdloc = "statusline"
 -- vim.o.clipboard = 'unnamedplus'
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.encoding = "UTF-8"
 vim.o.ruler = false
 vim.o.wrap = false
 vim.o.swapfile = true -- never disable this again
@@ -19,6 +22,7 @@ vim.o.conceallevel = 3
 vim.o.grepprg = "rg --vimgrep"
 
 vim.o.signcolumn = "yes"
+vim.o.backspace = "indent,eol,start"
 
 -- NOTE: for my future self, vim.o and vim.opt behave different in the sense that
 -- vim.o can only allow for basic usage, while vim.opt can do more stuff
