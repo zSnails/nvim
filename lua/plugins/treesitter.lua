7c624b188725ca2a21c90d0d627d3c42351f20d9
zSnails <erizojuan33@gmail.com>
2026-02-23 09:51:09 -0600

    refactor: use tree-sitter.config instead of tree-sitter.configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,29 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
-        -- { 'nvim-treesitter/playground' },
         { 'nvim-treesitter/nvim-treesitter-textobjects' }
     },
-    opts = {
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ['af'] = '@function.outer',
-                    ['if'] = '@function.inner',
-                    ['ac'] = '@class.outer',
-                    ['ic'] = '@class.inner',
-                    ['ak'] = '@parameter.outer',
-                    ['ik'] = '@parameter.inner',
-                    ['al'] = '@loop.outer',
-                    ['il'] = '@loop.inner',
-                }
+    config = function()
+        require("nvim-treesitter.config").setup({
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                        ['ak'] = '@parameter.outer',
+                        ['ik'] = '@parameter.inner',
+                        ['al'] = '@loop.outer',
+                        ['il'] = '@loop.inner',
+                    }
+                },
             },
-        },
-        highlight = {
-            enable = true,
-        },
-    },
-    -- main = "nvim-treesitter.configs",
+            highlight = {
+                enable = true,
+            },
+        })
+    end
 }
