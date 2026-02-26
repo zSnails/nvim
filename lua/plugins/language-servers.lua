8593351601f58b17371ff250828afe290673ed9d
zSnails <erizojuan33@gmail.com>
2026-02-25 22:06:36 -0600

    refactor: extract language server configurations into their own untracked file
    I should check if there's a way to add a file to the repo but not track
    any changes made to it

--- lua/plugins/language-servers.lua
+++ lua/plugins/language-servers.lua
@@ -1,114 +1,4 @@
 local on_attach = require('config.mappings').on_attach_func
-local is_windows = require('extras.os_detection').is_windows
-
-local function get_intelephense_path()
-    if is_windows() then
-        return "THIS_IS_WINDOWS_SO_NO_INTELEPHENSE_PATH"
-    end
-
-    return os.getenv("HOME") .. '/intelephense/key.txt'
-end
-
-local servers = {
-    glsl_analyzer = {},
-    zls = {},
-    ols = {},
-    hls = {},
-    air = {},
-    qmlls = {
-        cmd = { "qmlls6", "-E" }
-    },
-    intelephense = {
-        enable = not is_windows(),
-        init_options = {
-            licenceKey = get_intelephense_path()
-        },
-    },
-    -- typst_lsp = {
-    --     offset_encoding = "utf-8",
-    -- },
-    neocmake = {},
-    templ = {},
-    nimls = {},
-    lemminx = {},
-    eslint = {},
-    texlab = {},
-    kotlin_language_server = {},
-    -- volar = { filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' } },
-    cssls = {},
-    jdtls = {},
-    -- java_language_server = {
-    --     -- cmd = "/home/ayaka/java-language-server/dist/lang_server_linux.sh"
-    -- },
-    gopls = {},
-    emmet_ls = {
-        filetypes = { "astro", "css", "eruby", "html", "htmldjango", "javascriptreact", "less", "pug", "sass", "scss", "svelte", "typescriptreact", "vue", "templ" }
-    },
-    pyright = {
-        settings = {
-            python = {
-                analysis = {
-                    autoSearchPaths = true,
-                    -- diagnosticMode = "openFilesOnly",
-                    diagnosticMode = "workspace",
-                    useLibraryCodeForTypes = true
-                }
-            }
-        }
-    },
-    omnisharp = {
-        cmd = { "omnisharp" }
-    },
-    lua_ls = {
-        settings = {
-            Lua = {
-                -- runtime = {
-                --     version = 'LuaJIT',
-                -- },
-                diagnostics = {
-                    globals = { 'vim' },
-                },
-                workspace = {
-                    library = vim.api.nvim_get_runtime_file("", true),
-                    checkThirdParty = false,
-                },
-                telemetry = {
-                    enable = false,
-                },
-            }
-        }
-    },
-    clangd = {},
-    rust_analyzer = {},
-    ts_ls = {
-        init_options = {
-            plugins = {
-                {
-                    name = "@vue/typescript-plugin",
-                    location = "/usr/lib/node_modules/@vue/typescript-plugin",
-                    languages = { "vue" },
-                }
-            }
-        },
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-        single_file_support = false,
-    },
-    jsonls = {},
-    fsautocomplete = {},
-    gdscript = {
-        on_attach = function(client)
-            on_attach()
-            local _notify = client.notify
-            client.notify = function(method, params)
-                if method == 'workspace/didChangeConfiguration' then
-                    return
-                end
-                _notify(method, params)
-            end
-        end
-    },
-}
-
 
 return {
     'neovim/nvim-lspconfig',
@@ -117,7 +7,14 @@ return {
         'folke/neodev.nvim',
     },
     config = function(_, _)
-        -- local lsp_config = require('lspconfig')
+        -- NOTE: if pcall returns an error the message is stored in the servers variable
+        local success, servers = pcall(require, "config.local-servers")
+        if not success then
+            -- TODO: Si no hay local-servers que no haga nada
+            vim.api.nvim_err_writeln(vim.format("could not load 'config.local-servers': %s", servers))
+            return
+        end
+
         local nvim_lsp = require('cmp_nvim_lsp')
         local capabilities = nvim_lsp.default_capabilities()
         capabilities.diagnostic = true
