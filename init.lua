fb70f0d987c836bd614f0cd2462b7b46bb2785ad
zSnails <erizojuan33@gmail.com>
2026-02-25 21:45:35 -0600

    feat: make treesitter start automatically

--- init.lua
+++ init.lua
@@ -23,5 +23,15 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.db_ui_use_nerd_fonts = true
 
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = "*",
+    callback = function() 
+        local filetype = vim.bo.filetype
+        if filetype and filetype ~= "" then
+            pcall(vim.treesitter.start)
+        end
+    end,
+})
+
 require('config')
 require("lazy").setup("plugins")
