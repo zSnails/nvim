38fbdc98322bf962b25552a222cb6fbfe3f6f7b3
zSnails <erizojuan33@gmail.com>
2026-02-25 22:04:04 -0600

    refactor: move treesitter autocmd to the setup file

--- lua/config/setup.lua
+++ lua/config/setup.lua
@@ -46,4 +46,17 @@ vim.api.nvim_create_user_command("Grep", function()
     vim.cmd(":copen")
 end, {})
 
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = "*",
+    callback = function()
+        local filetype = vim.bo.filetype
+        if filetype and filetype ~= "" then
+            pcall(vim.treesitter.start)
+        end
+    end,
+})
+
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+
 vim.diagnostic.config({ virtual_text = true })
